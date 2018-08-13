5dea867e3d193c9fe850bc01afcfef9c2db4493f
isnotafunction <ivana.cosgrove@gmail.com>
2018-08-13 15:06:35 +0100

    adds error boundary

--- my-react-setup/src/components/App.js
+++ my-react-setup/src/components/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
+import ErrorBoundary from './error_boundary'
 
 export default class App extends React.Component{
   render(){
     return(
+      <ErrorBoundary>
       <h1>Hello React!</h1>
+      </ErrorBoundary>
     )
   }
 }
